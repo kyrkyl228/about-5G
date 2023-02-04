b59954f3c433f40e56c6689b22d2a482645e9f09
Vlaqikk <ilinskyvladisalv2002@mail.ru>
2023-02-04 17:03:41 +0500

    first commit

---
+++ js/mapW.js
@@ -0,0 +1,6 @@
+function mod(but, color){
+    $(".Null").css("fill", "#d2dcde");
+    $(".Null").css("background-color", "white");
+    $("."+but+"").css("fill", color);
+    $("."+but+"-back-but").css("background-color", color);
+}
