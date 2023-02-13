4b81092dbb5e93b854bf1ede69974d632e43256a
Vlaqikk <ilinskyvladisalv2002@mail.ru>
2023-02-13 19:06:56 +0500

    fiz v1.3

--- js/size.js
+++ js/size.js
@@ -2,6 +2,7 @@ function width(){
     if(document.documentElement.clientWidth <= 1100 - (window.innerWidth - document.documentElement.clientWidth)){
         let a = document.documentElement.clientWidth/1920;
         $("body").css("zoom", a);
+        alert(a);
     }else if(document.documentElement.clientWidth >= 1100 - (window.innerWidth - document.documentElement.clientWidth)){
         $("body").css("zoom", 1);
 
