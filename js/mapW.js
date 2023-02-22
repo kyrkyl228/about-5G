4779cfe1f40b2a1a99ea5995c47886b4144573f6
Vlaqikk <ilinskyvladisalv2002@mail.ru>
2023-02-22 20:30:10 +0500

    work with adaptation and map

--- js/mapW.js
+++ js/mapW.js
@@ -4,3 +4,22 @@ function mod(but, color){
     $("."+but+"").css("fill", color);
     $("."+but+"-back-but").css("background-color", color);
 }
+
+$(".map svg *").hover(function(event){
+    const a = 20;
+
+    $('.wrapper').append(`
+        <div id="map-info" style="overflow:hidden; display: flex; position:absolute; top: ${event.pageY / $(".wrapper").css("zoom") - a - parseInt($("#map-info").css("height"), 10)}px; left: ${event.pageX / $(".wrapper").css("zoom") - a - parseInt($("#map-info").css("width"), 10)}px; background-color: #f0f8ffc0; border-radius: 7px; width: 120px; height: 50px;">
+            <p style="margin: auto; font-size: 20px">${$(this).attr('name')}</p>
+        </div>
+    `);
+    $("body").mousemove(function(event){
+        if($("#map-info").css("top") != event.pageY / $(".wrapper").css("zoom") - a - parseInt($("#map-info").css("height"), 10) || $("#map-info").css("left") != event.pageX / $(".wrapper").css("zoom") - a - parseInt($("#map-info").css("width"), 10)){
+            $("#map-info").css("top", event.pageY / $(".wrapper").css("zoom") - a - parseInt($("#map-info").css("height"), 10));
+            $("#map-info").css("left", event.pageX / $(".wrapper").css("zoom") - a - parseInt($("#map-info").css("width"), 10));
+        }        
+    });
+
+}, function(){
+    $("#map-info").remove();
+});
--- js/size.js
+++ js/size.js
@@ -1,10 +1,10 @@
 function width(){
     if(document.documentElement.clientWidth <= 1100){
         let a = document.documentElement.clientWidth/1920;
-        $("body").css("zoom", a);
+        $(".wrapper").css("zoom", a);
         // alert(a);
     }else if(document.documentElement.clientWidth >= 1100){
-        $("body").css("zoom", 1);
+        $("wrapper").css("zoom", 1);
 
     }
 }
