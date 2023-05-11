e57d057058ef21774c1ec7968ca8e51ff8afc779
Vlaqikk <ilinskyvladisalv2002@mail.ru>
2023-05-11 17:43:59 +0500

    v2.20t

--- js/size.js
+++ js/size.js
@@ -1,15 +1,21 @@
 function width(){
-    if(document.documentElement.clientWidth <= 1100){
+    if(document.documentElement.clientWidth <= 750){
         let a = document.documentElement.clientWidth/1920;
-        $(".wrapper").css("zoom", a);
-        // alert(a);
-    }else if(document.documentElement.clientWidth >= 1100){
-        $("wrapper").css("zoom", 1);
+        $(".scale").css("zoom", a);
+        $(".head").css("zoom", a);
+        $(".scale").css("padding", "0px 10px");
+        $(".navi").css("padding", "0 10px");
+        $(".navi").css("width", "100%");
+    }else{
+        $(".scale").css("zoom", "");
+        $(".head").css("zoom", "");
+        $(".navi").css("width", "");
+        $(".scale").css("padding", "");
+        $(".navi").css("padding", "");
     }
 }
 
 $(document).ready(function(){
-    width();
     width();
 });
 
